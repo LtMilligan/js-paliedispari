115535158c17cb287f5981ee5f3c13d293fa2029
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 00:41:19 +0200

    [UPD] Created var for even or odd

---
+++ Pari_e_Dispari/js/script.js
@@ -0,0 +1,17 @@
+// ----------PARI E DISPARI------------
+
+//Dichiaro una variabile con un prompt per far scegliere all'utente se vuole giocare con il PARI o con il DISPARI
+
+let pariDispari = prompt('Scegli "pari" o "dispari"')
+
+//Dichiaro una variabile con un prompt per far inserire un numero all'utente da 1 a 5 facendo il parseInt del dato per averlo come intero e non come stringa
+
+//Creo una funzione il RETURN un numero generato randomicamente da 1 a 5 da assegnare successivamente come numero del computer
+
+// Dichiaro una variabile somma per andare a sommare il numero dell'utente a quello randomico del PC
+
+// Dichiaro un flag per il numero PARI impostandolo a TRUE
+
+// Creo una funzione per un IF per cambiare il flag in FALSE nel caso che la somma dei numeri sia DISPARI
+
+//Con un IF dichiaro le 2 condizioni di vittoria dell'utente, ovvero che lui abbia scelto PARI e che il flag sia TRUE oppure che lui abbia scelto DISPARI e che il flag sia FALSE, altrimenti nell ELSE c'è la vittoria del PC.
