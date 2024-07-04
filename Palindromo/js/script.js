b175724caecd9e574ba3f6d4cd518e9ba52380f4
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 00:33:05 +0200

    [UPD] Created var for user prompt for word and var for reverse word

---
+++ Palindromo/js/script.js
@@ -0,0 +1,9 @@
+// PALINDROMO
+
+// Dichiaro una variabile con assegnato un prompt per far inserire all'utente la parola da controllare
+
+let parola = prompt('Inserisci una parola');
+
+// Dichiaro una variabile per assegnarla successivamente con il reverse della parola scelta
+
+let reverseParola
